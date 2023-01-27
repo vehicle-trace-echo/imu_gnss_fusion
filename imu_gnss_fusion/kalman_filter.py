374e76ab26dbe85b985eecd9a286eafef7dfb976
sauravuprety21 <saurav.uprety1@ucalgary.ca>
2023-01-27 03:19:14 -0700

    ADDED setter for measurement uncertainty

--- imu_gnss_fusion/kalman_filter.py
+++ imu_gnss_fusion/kalman_filter.py
@@ -12,7 +12,7 @@ class KalmanFilter():
                 MAT_CNTRL: np.array,
                 MAT_OBSRVN: np.array,
                 MAT_PROCESS_NOISE: np.array,
-                MAT_MEAS_UNCERTT: np.array):
+                MAT_MEAS_UNCERT: np.array):
         '''
         Initialize the Kalman filter based on the minimum required parameters.
         
@@ -24,35 +24,36 @@ class KalmanFilter():
         '''
         self.set_state_transition_mat(MAT_STATE_TRANSTN)    
         self.set_control_mat(MAT_CNTRL)
-        self.set_observation_mat(MAT_CNTRL)
+        self.set_observation_mat(MAT_OBSRVN)
+        self.set_process_noise(MAT_PROCESS_NOISE)
 
     
 
     # Setter Method
     def set_state_transition_mat(self, MAT_STATE_TRANSTN : np.matrix) -> None:
         '''
-        Extract # of rows (@_ROWS_STATE_VECTR).
+        Extract # of rows (@ROWS_STATE_VECTR).
         Check for sqaure matrix.
-        Set @_MAT_STATE_TRANSTN.
+        Set @MAT_STATE_TRANSTN.
 
         :param @MAT_STATE_TRANSTN:          @MAT_STATE_TRANSTN
         '''
         _n = MAT_STATE_TRANSTN.shape[0]
-        _m = MAT_STATE_TRANSTN.shape[1]
+        _x = MAT_STATE_TRANSTN.shape[1]
 
-        if(_n == _m):
-            self._MAT_STATE_TRANSTN = MAT_STATE_TRANSTN
+        if(_n == _x):
+            self._MAT_STATE_TRANSTN = np.copy(MAT_STATE_TRANSTN)
             self._ROWS_STATE_VECTR = _n
             return
-        raise InvalidMatrixDimensions(f"State Transtion (F) must be a square matrix. Current dimensions: ({_n} x {_m})")
+        raise InvalidMatrixDimensions(f"State Transtion must be a square matrix. Current dimensions: ({_n} x {_x})")
     
 
     # Setter Method 
     def set_control_mat(self, MAT_CNTRL: np.matrix) -> None:
         '''
-        Extract # number of rows (@_ROWS_CNTRL_VECTR) 
-        Check for matrix dimensional consistency with @_MAT_STATE_TRANSTN.
-        Set @_MAT_CNTRL.
+        Extract # number of rows (@ROWS_CNTRL_VECTR) 
+        Check for matrix dimensional consistency with @MAT_STATE_TRANSTN.
+        Set @MAT_CNTRL.
 
         :param @MAT_CNTRL:                  @MAT_CNTRL
         '''
@@ -60,20 +61,20 @@ class KalmanFilter():
         _m = MAT_CNTRL.shape[1]
 
         if(not _n == self._ROWS_STATE_VECTR):
-            raise InconsistentMatrixDimensions(f"Control Matrix dimensions ({_n} x {_m}), inconsistent \
+            raise InconsistentMatrixDimensions(f"Control matrix dimensions ({_n} x {_m}), inconsistent \
                  with state transition matrix ({self._ROWS_STATE_VECTR} x {self._ROWS_STATE_VECTR}).\n \
-                    Control Matrix must have the dimensions ({self._ROWS_STATE_VECTR } x m)")
+                    Control matrix must have the dimensions ({self._ROWS_STATE_VECTR } x m)")
 
-        self._MAT_CNTRL = MAT_CNTRL
+        self._MAT_CNTRL = np.copy(MAT_CNTRL)
         self._ROWS_CNTRL_VECTR = _m
 
 
     # Setter Method
     def set_observation_mat(self, MAT_OBSRVN: np.matrix) -> None:
         '''
-        Extract # number of rows (@_ROWS_MEAS_VECTR)
-        Check for matrix dimensional consistency with @_MAT_STATE_TRANSTN
-        Set @_MAT_OBSRVN
+        Extract # number of rows (@ROWS_MEAS_VECTR)
+        Check for matrix dimensional consistency with @MAT_STATE_TRANSTN
+        Set @MAT_OBSRVN
 
         :param @MAT_OBSRVN:                 @MAT_OBSRVN
         '''
@@ -85,11 +86,56 @@ class KalmanFilter():
                  with state transition matrix ({self._ROWS_STATE_VECTR} x {self._ROWS_STATE_VECTR}).\n \
                     Observation Matrix must have the dimensions (k x {self._ROWS_STATE_VECTR })")
 
-        self._MAT_OBSRVN = MAT_OBSRVN
-        self._ROWS_OBSRVN_VECTR = _k
+        self._MAT_OBSRVN = np.copy(MAT_OBSRVN)
+        self._ROWS_MEAS_VECTR = _k
 
 
+    # Setter Method
+    def set_process_noise(self, MAT_PROCESS_NOISE: np.matrix) -> None:
+        '''
+        Extract # number of rows (@ROWS_STATE_VECTOR).
+        Check for matrix dimensional consistency with @MAT_STATE_TRANSTN
+        Set @MAT_PROCESS_NOISE
 
+        :param @MAT_PROCESS_NOISE:             @MAT_PROCESS_NOISE
+        '''
+        _n = MAT_PROCESS_NOISE.shape[0]
+        _x = MAT_PROCESS_NOISE.shape[1]
+
+        if(not _n == self._ROWS_STATE_VECTR):
+            raise InconsistentMatrixDimensions(f"Process Noise Matrix dimensions ({_n} x {_x}), inconsistent \
+                 with state transition matrix ({self._ROWS_STATE_VECTR} x {self._ROWS_STATE_VECTR}).\n \
+                    Process Noise Matrix must have the dimensions ({self._ROWS_STATE_VECTR}  x {self._ROWS_STATE_VECTR})")
+
+        if(not _n == _x):
+            raise InvalidMatrixDimensions(f"Process Noise must be a square matrix. \
+             Current dimensions: ({_n} x {_x})")
+
+        self._MAT_PROCESS_NOSE = np.copy(MAT_PROCESS_NOISE)
+
+
+    # Setter Method
+    def set_measurement_uncertainty(self, MAT_MEAS_UNCERT: np.matrix) -> None:
+        '''
+        Extract # number of rows (@ROWS_MEAS_VECTR). 
+        Check for matrix dimensional consistency with @MAT_OBSRVN
+        Set @MAT_MEAS_UNCERT
+
+        :param @MAT_MEAS_UNCERT:                @MAT_MEAS_UNCERT
+        '''
+        _k = MAT_MEAS_UNCERT.shape[0]
+        _x = MAT_MEAS_UNCERT.shape[1]
+
+        if(not _k == self._ROWS_STATE_VECTR):
+            raise InconsistentMatrixDimensions(f"Measurement Uncertainty Matrix dimensions ({_k} x {_x}), inconsistent \
+                with observation matrix ({self._ROWS_MEAS_VECTR} x {self._ROWS_MEAS_VECTR}).\n \
+                Measurement Uncertainty Matrix must have the dimensions ({self._ROWS_MEAS_VECTR}  x {self._ROWS_MEAS_VECTR})")
+
+        if(not _k == _x):
+            raise InvalidMatrixDimensions(f"Measurement Uncertainty must be a square matrix. \
+             Current dimensions: ({_k} x {_x})")
+            
+        self._MAT_MEAS_UNCERT = MAT_MEAS_UNCERT
 
 
 class InvalidMatrixDimensions(Exception):

6f003ba833a9c105dd6d8b7f4de26674af9ad0e6
sauravuprety21 <saurav.uprety1@ucalgary.ca>
2023-01-27 12:41:22 -0700

    INIT EKF

---
+++ imu_gnss_fusion/extended_kalman_filter.py
@@ -0,0 +1,11 @@
+import math
+import numpy as np
+from kalman_filter import KalmanFilter
+
+
+class ExtendedKalmanFilter(KalmanFilter):
+
+    def __init__(self):
+        # Create a new constructor for kalman filter 
+
+        pass

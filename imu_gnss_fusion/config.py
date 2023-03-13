0146e55ed8e40dd38def06f0460e4c92830aba01
sauravuprety21 <saurav.uprety1@ucalgary.ca>
2023-03-12 18:17:04 -0600

    ADDED cfg file to store IMU details and calibration params

---
+++ imu_gnss_fusion/config.py
@@ -0,0 +1,65 @@
+GRAV_MAGNTD = 9.8066
+
+IMU_CFG = {
+    'range' : {
+        'imu_max': '32767',
+        'imu_min': '-32768',
+        'units': 'ADC'
+             },  
+
+    'accel' : {
+        'range': {
+            'accel_min': -2,
+            'accel_max': 2,
+            'units': 'g'
+                    },
+
+        'bias' : {
+                'x' : -1.05954896e-01,
+                'y' : -1.60772046e-01,
+                'z' : -2.39030854e-02,
+                'units':'METERSPERSECONDS',
+                },
+        'misalign':
+                {
+                    'alpha_yz': 2.71122385e-05,
+                    'alpha_zy': -4.25645029e-03,
+                    'alpha_zx': 2.18810007e-03,
+                },
+        'scale':
+                {
+                'x': 9.96700502e-01,
+                'y': 9.96589996e-01, 
+                'z': 9.87936898e-01,
+                }
+            },
+        
+    'gyro' : {
+        
+        'range': {
+                'gyro_min' : -250,
+                'gyro_max' : 250,
+                'units': 'DEGREESPERSECOND'
+            },
+                        
+        'bias' :
+            {
+            'x': -0.6584513260335345,
+            'y': 1.202140150793541, 
+            'z':-0.1227491118389093,
+            'units': 'DEGREESPERSECONDS'
+            },
+        'misalign':
+                {
+            'gamma_yz': 0,
+            'gamma_zy': 0,
+            'gamma_zx': 0,
+                },
+        'scale':
+                {
+                'x': 1,
+                'y': 1, 
+                'z': 1,
+                }                
+            },
+}
--- imu_gnss_fusion/imu.py
+++ imu_gnss_fusion/imu.py
@@ -1,71 +1,121 @@
 import numpy as np
+from config import GRAV_MAGNTD, IMU_CFG
 
 
 class IMU():
     # IMU data read through 16-bit signed A/D converter
-    IMU_MAX = 32767
-    IMU_MIN = -32768
+    IMU_MAX = IMU_CFG['range']['imu_max']
+    IMU_MIN = IMU_CFG['range']['imu_min']
     IMU_RANGE = IMU_MAX - IMU_MIN
 
-    # Gyro reads in range +- 1000 dps
-    GYRO_MIN = -250
-    GYRO_MAX = 250
+    # Gyro reads in range +- 250 dps
+    GYRO_MIN = IMU_CFG['gyro']['range']['gyro_min']
+    GYRO_MAX = IMU_CFG['gyro']['range']['gyro_max']
     GYRO_RANGE = GYRO_MAX - GYRO_MIN
 
     #Pull up from data source
-    GRAV_MAGNTD = 9.8066
+    GRAV_MAGNTD = GRAV_MAGNTD
 
     # Accelorometer reads in +- 2g
-    ACCEL_MIN = -2
-    ACCEL_MAX = 2
+    ACCEL_MIN = IMU_CFG['accel']['range']['accel_min']
+    ACCEL_MAX = IMU_CFG['accel']['range']['accel_max']
     ACCEL_RANGE = ACCEL_MAX - ACCEL_MIN
 
     # Calibration Parameters
-    alfayz, alfazy, alfazx      =   0.0335241,0.00295066,0.00359227,
-    gamayz, gamazy, gamazx      =   0, 0, 0
-    sxa, sya, sza               =   1.00168651, 0.99445974, 0.98556708,
-    sxw, syw, szw               =   1, 1, 1
+    bxa, bya, bza               =   IMU_CFG['accel']['bias']['x'], \
+                                    IMU_CFG['accel']['bias']['y'], \
+                                    IMU_CFG['accel']['bias']['z']
+    
+    bxw, byw, bzw               =   IMU_CFG['gyro']['bias']['x'],\
+                                    IMU_CFG['gyro']['bias']['y'],\
+                                    IMU_CFG['gyro']['bias']['z']
+    
+    alfayz, alfazy, alfazx      =  IMU_CFG['accel']['misalign']['alpha_yz'],\
+                                    IMU_CFG['accel']['misalign']['alpha_zy'],\
+                                    IMU_CFG['accel']['misalign']['alpha_zx']
+    
+    gamayz, gamazy, gamazx      =  IMU_CFG['gyro']['misalign']['gamma_yz'],\
+                                    IMU_CFG['gyro']['misalign']['gamma_zy'],\
+                                    IMU_CFG['gyro']['misalign']['gamma_zx']
+    
+    sxa, sya, sza               =   IMU_CFG['accel']['scale']['x'], \
+                                    IMU_CFG['accel']['scale']['y'],\
+                                    IMU_CFG['accel']['scale']['z'],
 
+    sxw, syw, szw               =   IMU_CFG['gyro']['scale']['x'], \
+                                    IMU_CFG['gyro']['scale']['y'],\
+                                    IMU_CFG['gyro']['scale']['z']
 
-
+    # Accel axes misalignment corrections
     Ta = np.array([[1, -alfayz, alfazy],
                         [0, 1, -alfazx],
                         [0, 0, 1]])
+    
+    # Gyro axes misalignment corrections
     Tw = np.array([[1, -gamayz, gamazy],
                         [0, 1, -gamazx],
                         [0, 0, 1]])
 
+    # Accel scale factor corrections
     Ka = np.array([[sxa, 0, 0],
                         [0, sya, 0],
                         [0, 0, sza]])
 
+    # Gyro scale factor corrections
     Kw = np.array([[sxw, 0, 0],
                         [0, syw, 0],
                         [0, 0, szw]])
+    
+    # Accel biases 
+    ba = np.array([bxa, bya, bza])
+
+    # Gyro biases
+    bw = np.array([bxw, byw, bzw])
+
 
 
     @staticmethod
     def normalize_imu(ax, ay, az, gx, gy, gz)->np.ndarray:
+        '''
+        Given raw readings from ICM20948 16 bit ADC, convert bitfield to
+        physical readings (m/s^2 for accel and degrees/s for gyro).
+        '''
         accelNorm = IMU.GRAV_MAGNTD * ((np.array([ax, ay, az]) - IMU.IMU_MIN) \
                     / IMU.IMU_RANGE * IMU.ACCEL_RANGE + IMU.ACCEL_MIN)
         gyroNorm =  (np.array([gx, gy, gz]) - IMU.IMU_MIN) / IMU.IMU_RANGE * IMU.GYRO_RANGE + IMU.GYRO_MIN
         return np.concatenate((accelNorm, gyroNorm))
     
+
+
     @staticmethod
     def normalize_accel(rawAccelArr:np.ndarray)->np.ndarray:
+        '''
+        Given raw readings for accelerometer only, convert 16 bit ADC readings to m/s^2
+        '''
         return IMU.GRAV_MAGNTD * ((rawAccelArr - IMU.IMU_MIN) \
                     / IMU.IMU_RANGE * IMU.ACCEL_RANGE + IMU.ACCEL_MIN)
     
 
+
     @staticmethod
     def normalize_gyro(rawGyroArr:np.ndarray)->np.ndarray:
+        '''
+        Given raw readings for gyroscope only, convert 16 bit ADC readings to degrees/s
+        '''
         return (rawGyroArr - IMU.IMU_MIN) / IMU.IMU_RANGE * IMU.GYRO_RANGE + IMU.GYRO_MIN
     
 
     @staticmethod
-    def calibrate_accel(accelArr: np.ndarray, biasArr:np.ndarray)->np.ndarray:
+    def calibrate_accel(accelArr: np.ndarray, biasArr=ba)->np.ndarray:
+        '''
+        Calibrate accelerometer readings to remove deterministic errors
+        '''
         return np.matmul(np.matmul(IMU.Ta, IMU.Ka), (accelArr - biasArr))
     
+
     @staticmethod
-    def calibrate_gyro(gyroArr: np.ndarray, biasArr: np.ndarray)->np.ndarray:
+    def calibrate_gyro(gyroArr: np.ndarray, biasArr=bw)->np.ndarray:
+        '''
+        Calibrate gyroscope readings to remove deterministic errors
+        '''
         return np.matmul(np.matmul(IMU.Tw, IMU.Kw), (gyroArr - biasArr))
